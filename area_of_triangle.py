182cf8942cb6624c3fccf5c4d88808f134e63163
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-08 20:13:53 +0500

    Update area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -1,10 +1,11 @@
 '''Aswad Akram (Assignment #02)
 Area Of Triangle'''
 
+
 base = 25
 height = 10
 
-area = 0.5 * base * height
-print(area)
+area_triangle = 0.5*base*height
+print(area_triangle)
 
 #The area of the triangle will be 125.00 square units
