5195176b8b0dd0f5a0bef3d045d6974c0d1d397b
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-08 20:13:04 +0500

    Update area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,15 +1,12 @@
 '''Aswad Akram (Assignment #02)
 Area Of Circle'''
 
+
 import math
-
-def calculate_circle_area(radius):
-    area = math.pi * (radius ** 2)
-    return area
-
+ 
 radius = 15
 
-area = calculate_circle_area(15)
-print(f"The area of the circle is {area:.2f} square units")
+area_circle = math.pi*radius*radius
+print(area_circle)
 
 #The area of the circle will be 706.86 square units
