214951e8eed60a79aea5b6358012724ffac39a33
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-08 20:08:39 +0500

    Update area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -1,14 +1,10 @@
 '''Aswad Akram (Assignment #02)
 Area Of Triangle'''
 
-def calculate_triangle_area(base, height):
-    area = 0.5 * base * height
-    return area
+base = 25
+height = 10
 
-base = 25  
-height = 10  
-
-area = calculate_triangle_area(25,10)
-print(f"The area of the triangle is {area:.2f} square units")
+area = 0.5 * base * height
+print(area)
 
 #The area of the triangle will be 125.00 square units
